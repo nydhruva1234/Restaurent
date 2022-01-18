fb3d8652d8029e80d888a50e9372b945b3f95fa6
nani <nani.nydhruva@gmail.com>
2022-01-18 12:28:55 +0530

    third commit

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ const ejs=require('ejs')
 const expresslayout=require("express-ejs-layouts")
 const port=process.env.port||3000
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
+
 app.use(express.static('frontend'))
 app.use(expresslayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
 
 app.listen(port, function(){
     console.log(`Listening to the port ${port}`)
